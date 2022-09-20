75567a44dd19ba622ed9c1589ff2cd4f94311b7b
vi <atel01v-f0@yahoo.com>
2022-09-20 13:55:15 +0200

    TFM notebooks y logs

---
+++ scripts/config.py
@@ -0,0 +1,15 @@
+#config.py
+
+#CONSTANTES
+#ruta donde desargar ficheros
+ruta_temp = '../_temp/'
+
+#ACCESO A BASE DE DATOS
+db_host =  'localhost'
+db_port = 3306
+db_user = 'root' 
+db_pwd = '********'  #<< 
+db_name = 'TAQUILLA'
+
+#THE MOVIE DATABASE API
+api_key = "****************"   #<< 
